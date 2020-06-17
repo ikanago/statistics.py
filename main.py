782aaff76e78ac43123768b976a03b2e876e868e
ikanago <28985004+ikanago@users.noreply.github.com>
2020-06-17 11:34:09 +0900

    ✨ Read data from a csv file

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from scipy import stats
 import numpy as np
+from scipy import stats
+import argparse
+import csv
 from decimal import Decimal, ROUND_HALF_UP
 import math
-import argparse
 
 def normarize_variance(variance: float, n: int):
     return math.sqrt(variance / n)
@@ -74,13 +75,18 @@ def main():
     parser.add_argument("-i", "--input", type=str, default="data.csv", help="入力データの記録されたCSVファイルへのパス(default: data.csv).")
     parser.add_argument("-c", "--confidence", type=float, default=0.95, help="信頼係数(default: 0.95)")
     args = parser.parse_args()
+    input_file = args.input
     confidence = args.confidence
     if confidence <= 0 or confidence >= 1:
         import sys
         print("信頼係数は(0, 1)の範囲で指定してください．", file=sys.stderr)
         exit()
     
-    data = np.array([12.4, 13.5, 12.7, 14.1, 13.8, 14.1, 12.0, 12.8, 13.1, 15.4])
+    with open(input_file) as f:
+        reader = csv.reader(f)
+        contents = [[float(v) for v in row] for row in reader]
+        data = np.array(contents[0])
+    
     data_size = len(data)
     sample_mean = data.mean()
     sample_variance = data.var()
