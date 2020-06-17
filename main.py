cb19a50bdc5e48c50c21672bffe845a3ae84698e
ikanago <28985004+ikanago@users.noreply.github.com>
2020-06-17 11:26:10 +0900

    ✨ Recieve confidence from command line argument

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from scipy import stats
 import numpy as np
 from decimal import Decimal, ROUND_HALF_UP
 import math
+import argparse
 
 def normarize_variance(variance: float, n: int):
     return math.sqrt(variance / n)
 
-def interval_estimate_mean_with_pop_variance(sample_mean: float, pop_variance: float, n: int):
+def interval_estimate_mean_with_pop_variance(sample_mean: float, pop_variance: float, n: int, confidence: float):
     """
     母平均の95%信頼区間を求める．母分散は既知とする．
 
@@ -16,14 +17,17 @@ def interval_estimate_mean_with_pop_variance(sample_mean: float, pop_variance: f
     pop_variance: 母分散
 
     n: データ数
+
+    confidence: 信頼係数
+
     ## Returns
-    (bottom, top): 推定した95%信頼区間
+    (bottom, top): 推定した信頼区間の下限と上限
     """
     normarized_varianse = normarize_variance(pop_variance, n)
-    bottom, top = stats.norm.interval(0.95, loc=sample_mean, scale=normarized_varianse)
+    bottom, top = stats.norm.interval(confidence, loc=sample_mean, scale=normarized_varianse)
     return (bottom, top)
 
-def interval_estimate_mean_without_pop_variance(sample_mean: float, sample_varianse: float, n: int):
+def interval_estimate_mean_without_pop_variance(sample_mean: float, sample_variance: float, n: int, confidence: float):
     """
     母平均の95%信頼区間を求める．母分散は未知とする．
 
@@ -33,15 +37,18 @@ def interval_estimate_mean_without_pop_variance(sample_mean: float, sample_varia
     sample_variance: 標本分散
 
     n: データ数
+
+    confidence: 信頼係数
+
     ## Returns
-    (bottom, top): 推定した95%信頼区間
+    (bottom, top): 推定した信頼区間の下限と上限
     """
     df = n - 1
-    normarized_variance = normarize_variance(sample_varianse, df)
-    bottom, top = stats.t.interval(0.95, loc=sample_mean, scale=normarized_variance, df=df)
+    normarized_variance = normarize_variance(sample_variance, df)
+    bottom, top = stats.t.interval(confidence, loc=sample_mean, scale=normarized_variance, df=df)
     return (bottom, top)
 
-def interval_estimate_variance(sample_variance: float, n: int):
+def interval_estimate_variance(sample_variance: float, n: int, confidence: float):
     """
     母平均の95%信頼区間を求める．母分散は未知とする．
 
@@ -49,18 +56,31 @@ def interval_estimate_variance(sample_variance: float, n: int):
     sample_variance: 標本分散
 
     n: データ数
+
+    confidence: 信頼係数
+
     ## Returns
-    (bottom, top): 推定した95%信頼区間
+    (bottom, top): 推定した信頼区間の下限と上限
     """
     df = n - 1
-    chi2_lower = Decimal(stats.chi2.ppf(0.975, df)).quantize(Decimal('0.001'), rounding=ROUND_HALF_UP)
-    chi2_higher = Decimal(stats.chi2.ppf(0.025, df)).quantize(Decimal('0.001'), rounding=ROUND_HALF_UP)
+    chi2_lower = Decimal(stats.chi2.ppf((1 + confidence) / 2.0, df)).quantize(Decimal('0.001'), rounding=ROUND_HALF_UP)
+    chi2_higher = Decimal(stats.chi2.ppf((1 - confidence) / 2.0, df)).quantize(Decimal('0.001'), rounding=ROUND_HALF_UP)
     bottom = n * sample_variance / float(chi2_lower)
     top = n * sample_variance / float(chi2_higher)
     return (bottom, top)
 
 def main():
-    data = np.array([100, 70, 30, 60, 50])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type=str, default="data.csv", help="入力データの記録されたCSVファイルへのパス(default: data.csv).")
+    parser.add_argument("-c", "--confidence", type=float, default=0.95, help="信頼係数(default: 0.95)")
+    args = parser.parse_args()
+    confidence = args.confidence
+    if confidence <= 0 or confidence >= 1:
+        import sys
+        print("信頼係数は(0, 1)の範囲で指定してください．", file=sys.stderr)
+        exit()
+    
+    data = np.array([12.4, 13.5, 12.7, 14.1, 13.8, 14.1, 12.0, 12.8, 13.1, 15.4])
     data_size = len(data)
     sample_mean = data.mean()
     sample_variance = data.var()
@@ -70,13 +90,13 @@ def main():
     print("不偏分散: {}".format(unbiased_variance))
 
     pop_variance = 625
-    bottom, top = interval_estimate_mean_with_pop_variance(sample_mean, pop_variance, data_size)
+    bottom, top = interval_estimate_mean_with_pop_variance(sample_mean, pop_variance, data_size, confidence)
     print("母平均の95%信頼区間(母分散既知): [ {}, {} ]".format(bottom, top))
 
-    bottom, top = interval_estimate_mean_without_pop_variance(sample_mean, sample_variance, data_size)
+    bottom, top = interval_estimate_mean_without_pop_variance(sample_mean, sample_variance, data_size, confidence)
     print("母平均の95%信頼区間(母分散未知): [ {}, {} ]".format(bottom, top))
 
-    bottom, top = interval_estimate_variance(sample_variance, data_size)
+    bottom, top = interval_estimate_variance(sample_variance, data_size, confidence)
     print("母分散の95%信頼区間            : [ {}, {} ]".format(bottom, top))
 
 
