02e3e74db95cda2a70423da66be41e29169699c9
ikanago <28985004+ikanago@users.noreply.github.com>
2020-06-19 20:30:12 +0900

    📂 Extract `desc` to a file

--- describe.py
+++ describe.py
@@ -1,3 +1,5 @@
+import csv
+import click
 import numpy as np
 
 
@@ -20,3 +22,28 @@ def describe(data: list) -> (int, float, float, float):
     sample_variance = data.var()
     unbiased_variance = data.var(ddof=1)
     return (len(data), sample_mean, sample_variance, unbiased_variance)
+
+
+@click.command()
+@click.option("-f", "--file", type=click.Path(exists=True), default="data.csv")
+def cmd(file):
+    with open(file) as f:
+        reader = csv.reader(f)
+        # 2-dimensional array, but each row can be different number of elements.
+        contents = [[float(v) for v in row] for row in reader]
+        for i, row in enumerate(contents):
+            length, sample_mean, sample_variance, unbiased_variance = describe(
+                row)
+            print("系列{}".format(i + 1))
+            print("データ数: {}".format(length))
+            print("標本平均: {}".format(sample_mean))
+            print("標本分散: {}".format(sample_variance))
+            print("不偏分散: {}\n".format(unbiased_variance))
+
+
+def main():
+    cmd()
+
+
+if __name__ == "__main__":
+    main()
