5d3842963358fcc35e02b32b42a9de6a318b97c9
ikanago <28985004+ikanago@users.noreply.github.com>
2020-06-26 17:13:56 +0900

    ✨ Implement correlation test

--- stats_test_correl.py
+++ stats_test_correl.py
@@ -16,7 +16,7 @@ def test_no_correl(n: int, r: float, significance: float) -> (bool, float):
     `significance`: 有意水準  
 
     ## Returns  
-    `is_reject`: 帰無仮説が棄却されるかどうか
+    `is_reject`: 帰無仮説が棄却されるかどうか  
     `t`: 実現値
     """
 
@@ -30,12 +30,38 @@ def test_no_correl(n: int, r: float, significance: float) -> (bool, float):
     return (is_reject, t)
 
 
+def test_correl(n: int, target_r: float, r: float, significance: float) -> (bool, float):
+    """
+    相関係数に関するz検定を行う．
+
+    ## Parameters  
+    `n`: 標本の大きさ  
+    `target_r`: 帰無仮説で等しいと仮定する相関係数  
+    `r`: 標本相関係数  
+    `significance`: 有意水準  
+
+    ## Returns  
+    `is_reject`: 帰無仮説が棄却されるかどうか  
+    `z`: 実現値
+    """
+
+    x = math.log((1 + r) / (1 - r)) / 2.0
+    xi = math.log((1 + target_r) / (1 - target_r)) / 2.0
+    z = (x - xi) * math.sqrt(n - 3)
+    bottom = stats.norm.ppf(significance / 2)
+    top = stats.norm.ppf(1 - significance / 2)
+    side = stats_test.side_from_str("double")
+    is_reject = stats_test.is_reject(
+        z, side, bottom, top, None, None)
+    return (is_reject, z)
+
+
 @click.command()
 @click.option("-n", type=int, default=0, help="標本の大きさ")
-@click.option("-r", type=float, default=0, help="標本相関係数")
 @click.option("-h", "--hypothesis", type=float, default=0, help="帰無仮説で等しいと仮定する相関係数")
+@click.option("-r", type=float, default=0, help="標本相関係数")
 @click.option("-l", "--level", type=float, default=0.05, help="有意水準(default: 0.05)")
-def cmd(n: int, r: float, hypothesis: float, level: float):
+def cmd(n: int, hypothesis: float, r: float, level: float):
     if level <= 0 or level >= 1:
         import sys
         print("信頼係数は(0, 1)の範囲で指定してください．", file=sys.stderr)
@@ -44,9 +70,13 @@ def cmd(n: int, r: float, hypothesis: float, level: float):
     if hypothesis == 0.0:
         (is_reject, test_stat) = test_no_correl(n, r, level)
     else:
-        pass
+        if hypothesis >= 1.0:
+            import sys
+            print("相関係数1の帰無仮説における検定はできません．", file=sys.stderr)
+            exit()
+        (is_reject, test_stat) = test_correl(n, hypothesis, r, level)
 
-    print(stats_test.show_result(is_reject, test_stat, "μ", hypothesis))
+    print(stats_test.show_result(is_reject, test_stat, "r", hypothesis))
 
 
 def main():
--- tests/test_stats_test_correl.py
+++ tests/test_stats_test_correl.py
@@ -13,3 +13,12 @@ class TestStatsTestCorrel(TestCase):
         (is_reject, test_stat) = stats_test_correl.test_no_correl(n, r, sigificance)
         self.assertFalse(is_reject)
         self.assertAlmostEqual(test_stat, 1.804, places=3)
+
+    def test_stats_test_correl(self):
+        n = 48
+        target_r = 0.7
+        r = 0.648
+        sigificance = 0.05
+        (is_reject, test_stat) = stats_test_correl.test_correl(n, target_r, r, sigificance)
+        self.assertFalse(is_reject)
+        self.assertAlmostEqual(test_stat, -0.640, places=3)
